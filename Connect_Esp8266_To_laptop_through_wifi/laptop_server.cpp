5fd4a060953e966286b4b5381eeacc70b230e7c7
quan <quan.dhww@gmail.com>
2025-04-07 22:39:51 +0700

    Update code server

--- Connect_Esp8266_To_laptop_through_wifi/laptop_server.cpp
+++ Connect_Esp8266_To_laptop_through_wifi/laptop_server.cpp
@@ -1,3 +1,10 @@
+/*
+TODO:
+Create a client from laptop and send command from there
+Connection is not stable
+*/
+
+
 #include <iostream>
 #include <cstring>
 #include <unistd.h>
@@ -9,16 +16,154 @@
 #include <condition_variable>
 #include <poll.h>
 #include <chrono>
+#include "include/json.hpp"
+using json = nlohmann::json;
 
+#define STATIC_SERVER_ETHERNET_IP_ADDRESS  "192.168.99.200"
+#define STATIC_SERVER_LAN0_IP_ADDRESS "192.168.50.1"
+#define STATIC_ANY_IP_ADDRESS "0.0.0.0"
 #define PORT 1369
 #define BUFFER_SIZE 1024
 #define CURRENT_TIME std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count()%1000000000
 #define MAX_CLIENT_IN_QUEUE 3
 #define MAX_THEAD_TO_HANDLE_CLIENT 1
+#define TIMEOUT_SECONDS 3
 
 std::queue<int> g_clientHandlerQueue{};
 std::mutex g_m_g_clientHandlerQueue{};
 std::condition_variable g_cv_g_clientHandlerQueue{};
+
+std::string GetMsgFromJson(char* buffer)
+{
+    // Parse the JSON message
+    try {
+        json receivedJson = json::parse(buffer);
+
+        // Process the parsed JSON
+        if (receivedJson.contains("cmd")) {
+            std::string command = receivedJson["cmd"];
+            std::cout << "Received command: " << command << "\n";
+            return command;
+        }
+    } catch (const std::exception& e) {
+        std::cout << "Error parsing JSON: " << e.what() << "\n";
+    }
+    return "";
+}
+
+bool IsMsgSentToController(int& clientHandler)
+{
+    char buffer[BUFFER_SIZE] = {0};
+    fd_set readfds;
+    struct timeval timeout;
+    timeout.tv_sec = TIMEOUT_SECONDS;
+    timeout.tv_usec = 0;
+
+    FD_ZERO(&readfds);
+    FD_SET(clientHandler, &readfds);
+
+    int activity = select(clientHandler + 1, &readfds, NULL, NULL, &timeout);
+
+    if (activity < 0) {
+        std::cout << "Error in select function\n";
+    } else if (activity == 0) {
+        std::cout << "Timeout reached, no data received from client within " << TIMEOUT_SECONDS << " seconds\n";
+    } else {
+        if (FD_ISSET(clientHandler, &readfds)) {
+            // Read data (non-blocking)
+            int readStatus = read(clientHandler, buffer, BUFFER_SIZE);
+            if (readStatus == -1) {
+                std::cout << "Error: Cannot read data from client\n";
+            } else if (readStatus == 0) {
+                std::cout << "Client closed the connection\n";
+            } else {
+                // Check if the last character is '\n', indicating end of message
+                if (buffer[readStatus - 1] == '\n') {
+                    buffer[readStatus - 1] = '\0';
+                    std::cout << "Received JSON message: " << buffer << "\n";
+                    if(GetMsgFromJson(buffer) == "ACK")
+                    {
+                        return true;
+                    }
+                } else {
+                    std::cout << "Message does not end with newline or is incomplete.\n";
+                }
+            }
+        }
+    }
+    return false;
+}
+
+std::string CreateMsgJsonToSend(std::string msg)
+{
+    // Create JSON message
+    json j;
+    j["cmd"] = msg;
+    // Serialize the JSON to string
+    std::string json_message = j.dump();
+    json_message += '\n';
+    return json_message;
+}
+
+bool CheckController(char* buffer)
+{
+    std::string temp = "";
+    for(int i=0; i<10; ++i)
+    {
+        if(buffer[i] == '/')
+        {
+            if(temp == "ESP") return true;
+            else return false;
+        }
+        temp += buffer[i];
+    }
+    return false;
+}
+
+bool HandleController(int& clientHandler, std::thread::id thread_id)
+{
+    std::cout << "In HandleController\n";
+    struct sockaddr_in clientAddress;
+    socklen_t clientAddress_len = sizeof(clientAddress);
+    getpeername(clientHandler, reinterpret_cast<struct sockaddr*>(&clientAddress), &clientAddress_len);
+    char clientIpAdress[INET_ADDRSTRLEN];
+    if (inet_ntop(AF_INET, &clientAddress.sin_addr, clientIpAdress, INET_ADDRSTRLEN) == NULL) 
+    {
+        std::cout << CURRENT_TIME << ": " << "Can not convert ip address of client\n";
+        return true;
+    }
+    std::cout << CURRENT_TIME << ": " << "Thread - {" << thread_id << "} controlling client - " << std::string(clientIpAdress)<< "/" << clientAddress.sin_port << '\n';
+
+    char buffer[BUFFER_SIZE] = {0};
+    std::cout << CURRENT_TIME << ": " << "Thread - {" << thread_id << "}" << " waiting for command...\n";
+    std::string in = "ON";
+    while(true)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        if(in == "ON")
+        {
+            in = "OFF";
+        }
+        else
+        {
+            in = "ON";
+        }
+        std::string msg = CreateMsgJsonToSend(in);
+        int blockingMode = 0;
+        if(send(clientHandler, msg.c_str(), msg.size(), blockingMode) == -1)
+        {
+            std::cout << CURRENT_TIME << ": " << "Can not send msg back to client!";
+        }
+        std::cout << CURRENT_TIME << ": " << "Thread - {" << thread_id << "}" << " sent command {" << msg << "} to controller\n";
+        if(IsMsgSentToController(clientHandler) == true) continue;
+        else
+        {
+            std::cout << CURRENT_TIME << ": " << "No ACK from CONTROLLER" << '\n';
+            return true;
+        }
+    }
+    return false;
+}
 
 void HandleClient(int& clientHandler, std::thread::id thread_id)
 {
@@ -59,6 +204,10 @@ void HandleClient(int& clientHandler, std::thread::id thread_id)
             flag = true;
         }
         std::cout << CURRENT_TIME << ": " << "Thread - {" << thread_id << "}" << " sent ACK\n";
+        if(CheckController(buffer) == true)
+        {
+            flag = HandleController(clientHandler, thread_id);
+        }
     }
     if(flag == true)
     {
@@ -99,11 +248,10 @@ int main()
     {
         clientHandlerThreadPool.push_back(std::thread(ThreadForHandlingClient));
     }
-
-    std::string staticServerEthernetIpAddress = "192.168.99.200";
+    std::string serverIpAddress = STATIC_ANY_IP_ADDRESS;
     struct sockaddr_in socketServerAddress;
     socketServerAddress.sin_family = AF_INET;
-    if(inet_pton(AF_INET, staticServerEthernetIpAddress.c_str(), &socketServerAddress.sin_addr) <= 0)
+    if(inet_pton(AF_INET, serverIpAddress.c_str(), &socketServerAddress.sin_addr) <= 0)
     {
         std::cerr << "Can not set ip address to socketServerAddress!";
         exit(EXIT_FAILURE);
